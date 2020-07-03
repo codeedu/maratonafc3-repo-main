ccf733f1dbfdc84fbdb5e8e68c5b03ab5fa9619c
Luiz Carlos <argentinaluiz@gmail.com>
2020-07-03 16:47:13 -0300

    Change STATIC_URL

--- commerce-admin/commerce_admin/settings.py
+++ commerce-admin/commerce_admin/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/admin/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
